546620d4d90832e54b8aebafa8255ba0c46a4fb3
rynhrd-nlz <reynhardhalaka@gmail.com>
2025-08-20 00:16:37 +0700

    Add .next/static/chunks/src_app_home_page_tsx_a993527f._.js 1755623797135

---
+++ .next/static/chunks/src_app_home_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_home_page_tsx_9744e9ac._.js",
+  "static/chunks/node_modules_next_dist_compiled_react_26ec58f1._.js"
+],
+    source: "dynamic"
+});
